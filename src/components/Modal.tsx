b6283df5cb27ee717cfb5f0bef4545f7ce0a8a93
topgun0209 <topgun0209@naver.com>
2022-04-20 20:59:58 +0900

    Modal 창, mockup 구현

---
+++ src/components/Modal.tsx
@@ -0,0 +1,42 @@
+import classNames from 'classnames';
+import React, { MouseEvent, ReactChild, useRef } from 'react';
+import '../scss/modal.scss';
+
+type TProps = {
+    title: string;
+    show: Boolean;
+    close: () => void;
+    children?: ReactChild;
+};
+const Modal = ({ title, show, close, children }: TProps) => {
+    const background = useRef(null);
+
+    const handleClickOutside = (props: MouseEvent<HTMLElement>) => {
+        // close when click outside
+        props.target === background.current && close();
+    };
+
+    return (
+        <div id="modal">
+            {show && (
+                <div
+                    ref={background}
+                    className={classNames('background', { show: show })}
+                    onClick={(e) => handleClickOutside(e)}
+                >
+                    <div className={classNames('popup', { show: show })}>
+                        <div className="title">
+                            <h2>{title}</h2>
+                        </div>
+                        <div className="close" onClick={close}>
+                            &times;
+                        </div>
+                        <div className="content">{children}</div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Modal;
---
+++ src/components/ModalTest.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ModalTest = () => {
+    return (
+        <div className="modal" style={{ backgroundColor: 'yellow', minHeight: '80vh' }}>
+            modal!
+        </div>
+    );
+};
+
+export default ModalTest;
