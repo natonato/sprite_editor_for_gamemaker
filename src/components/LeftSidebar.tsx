4ffb49c523b5dcf8884617f298c598dd58f02096
topgun0209 <topgun0209@naver.com>
2022-04-20 21:04:21 +0900

    Page 2 수정
    - Page 2 -> 1 링크 추가
    - Page2 left/right 구분

---
+++ src/components/LeftSidebar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LeftSidebar = () => {
+    return <div style={{ backgroundColor: 'wheat', flex: 1 }}>LeftSide</div>;
+};
+
+export default LeftSidebar;
---
+++ src/components/RightSidebar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const RightSidebar = () => {
+    return (
+        <div
+            style={{
+                backgroundColor: 'seagreen',
+                flex: 1,
+            }}
+        >
+            RightSide
+        </div>
+    );
+};
+
+export default RightSidebar;
