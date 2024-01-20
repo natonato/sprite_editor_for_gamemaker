2881e25dee724746267cebd27984f09d20a51ea1
HyeonGeon Lee <topgun0209@naver.com>
2024-01-20 23:16:05 +0900

    :sparkles: 타일을 아래로 한 줄 미는 기능 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -11,6 +11,7 @@ const MainPage = () => {
   const imageRef = useRef<HTMLImageElement>(null);
 
   const [tileSize, setTileSize] = useState({ width: 16, height: 16 });
+  const [isMoveRight, setIsMoveRight] = useState(true);
 
   useEffect(() => {
     const width = Number(localStorage.getItem("width")) || 16;
@@ -20,13 +21,27 @@ const MainPage = () => {
   }, []);
 
   useEffect(() => {
-    if (!file?.img) {
+    if (!file?.img || !canvasRef.current) {
       return;
     }
-
     const ctx = canvasRef.current?.getContext("2d");
 
-    ctx?.clearRect(0, 0, file?.img.width * 2, file?.img.height);
+    ctx?.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+
+    if (!isMoveRight) {
+      ctx?.drawImage(
+        file?.img,
+        0,
+        0,
+        file?.img.width,
+        file?.img.height,
+        0,
+        tileSize.height,
+        file?.img.width,
+        file?.img.height
+      );
+      return;
+    }
 
     ctx?.drawImage(
       file?.img,
@@ -51,61 +66,90 @@ const MainPage = () => {
       file?.img.width,
       tileSize.height
     );
-  }, [tileSize.width, tileSize.height, file]);
+  }, [tileSize.width, tileSize.height, file, isMoveRight]);
 
   return (
-      <form>
     <div className="mainPage">
+      Move Direction :
+      <label>
         <input
-          type="file"
-          accept="image/*"
-          onChange={(e) => {
-            e.preventDefault();
-
-            if (!e.target.files) {
-              return;
-            }
-
-            const reader = new FileReader();
-            const file = e.target.files[0];
-            reader.onload = () => {
-              const img = new Image();
-              img.src = reader.result as string;
-
-              setFile({
-                name: file.name,
-                imageUrl: reader.result as string,
-                img: img,
-              });
-            };
-            reader.readAsDataURL(file);
+          type="radio"
+          name={"type"}
+          defaultChecked={true}
+          onClick={() => {
+            setIsMoveRight(true);
           }}
         />
-        <br />
-        <label> Tile Width : </label>
+        Right
+      </label>
+      <label>
         <input
-          type="number"
-          value={tileSize.width.toString()}
-          onChange={(e) => {
-            localStorage.setItem("width", e.target.value);
+          type="radio"
+          name={"type"}
+          onClick={() => {
+            setIsMoveRight(false);
+          }}
+        />
+        Down
+      </label>
+      <br />
+      <input
+        type="file"
+        accept="image/*"
+        onChange={(e) => {
+          e.preventDefault();
 
-            setTileSize((prev) => ({ ...prev, width: Number(e.target.value) }));
-          }}
-        />
-        <br />
-        <label> Tile Height : </label>
-        <input
-          type="number"
-          value={tileSize.height.toString()}
-          onChange={(e) => {
-            localStorage.setItem("height", e.target.value);
-            setTileSize((prev) => ({
-              ...prev,
-              height: Number(e.target.value),
-            }));
-          }}
-        />
-      </form>
+          if (!e.target.files) {
+            return;
+          }
+
+          const reader = new FileReader();
+          const file = e.target.files[0];
+          reader.onload = () => {
+            const img = new Image();
+            img.src = reader.result as string;
+
+            setFile({
+              name: file.name,
+              imageUrl: reader.result as string,
+              img: img,
+            });
+          };
+          reader.readAsDataURL(file);
+        }}
+      />
+      {isMoveRight && (
+        <>
+          <br />
+          <label> Tile Width : </label>
+          <input
+            type="number"
+            value={tileSize.width.toString()}
+            onChange={(e) => {
+              localStorage.setItem("width", e.target.value);
+
+              setTileSize((prev) => ({
+                ...prev,
+                width: Number(e.target.value),
+              }));
+            }}
+          />
+        </>
+      )}
+      <br />
+      <label> Tile Height : </label>
+      <input
+        type="number"
+        value={tileSize.height.toString()}
+        onChange={(e) => {
+          localStorage.setItem("height", e.target.value);
+          setTileSize((prev) => ({
+            ...prev,
+            height: Number(e.target.value),
+          }));
+        }}
+      />
+      <br />
       {file?.imageUrl && (
         <>
           <img
@@ -118,8 +162,8 @@ const MainPage = () => {
           <canvas
             ref={canvasRef}
             id="image"
-            width={file.img.width + tileSize.width}
-            height={file.img.height}
+            width={file.img.width + (isMoveRight ? tileSize.width : 0)}
+            height={file.img.height + (!isMoveRight ? tileSize.height : 0)}
           />
           <br />
           <button
