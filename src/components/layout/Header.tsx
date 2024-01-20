c93281e22b0269d7764243de489a7cfc83276de0
HyeonGeon Lee <topgun0209@naver.com>
2024-01-20 22:45:50 +0900

    :art: 스타일 파일 생성

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "scss/header.scss";
 
 const Header = () => {
   return (
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
+import "scss/mainPage.scss";
 
 const MainPage = () => {
   const [file, setFile] = useState<{
