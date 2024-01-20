1c073cce7150617a67af3739f1c7346290fb3604
HyeonGeon Lee <topgun0209@naver.com>
2024-01-21 01:25:17 +0900

    :ok_hand: default hompage 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'index.scss';
-import App from 'App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "index.scss";
+import App from "App";
+import { BrowserRouter } from "react-router-dom";
 
-const rootNode: HTMLElement = document.getElementById('root') as HTMLElement;
+const rootNode: HTMLElement = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(rootNode).render(
-    <React.StrictMode>
-        <BrowserRouter basename="/react-frame">
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/tile_sprite_editor_for_gamemaker">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
