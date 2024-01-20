d452ffab6aa1cfbbd6c45ece8bb92dac5e234053
HyeonGeon Lee <topgun0209@naver.com>
2024-01-20 23:19:14 +0900

    :art: 깃허브버튼 텍스트, 스타일 조정

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,13 +4,19 @@ import "scss/footer.scss";
 const Footer = () => {
   return (
     <div className="footer">
-      <a href="https://github.com/natonato" target="_blank" rel="noreferrer">
+      <a
+        href="https://github.com/natonato"
+        target="_blank"
+        rel="noreferrer"
+        className="githubBtn"
+      >
         <img
           src={require(`image/github-mark.png`)}
-          width="20px"
-          height="20px"
+          width="40px"
+          height="40px"
           alt=""
         />
+        Create By natonato
       </a>
     </div>
   );
