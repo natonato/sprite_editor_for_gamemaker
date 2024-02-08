dae074af23dae28c9694aeb0b3346a08594f88b0
HyeonGeon Lee <topgun0209@naver.com>
2024-02-08 22:10:53 +0900

    Github Page 홈페이지 업데이트

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import "index.scss";
 import App from "App";
 import { BrowserRouter } from "react-router-dom";
+import json from "../package.json";
 
 const rootNode: HTMLElement = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(rootNode).render(
   <React.StrictMode>
-    <BrowserRouter basename="/tile_sprite_editor_for_gamemaker">
+    <BrowserRouter basename={json.homepage}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
